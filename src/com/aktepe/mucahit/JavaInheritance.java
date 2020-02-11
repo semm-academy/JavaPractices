56bb88fa071b62b99b8374305b63ae5b3ddee28b
theizmir <onurkasaburi@gmail.com>
2020-02-11 10:25:01 -0600

    second update

--- src/com/aktepe/mucahit/JavaInheritance.java
+++ src/com/aktepe/mucahit/JavaInheritance.java
@@ -2,7 +2,9 @@ package com.aktepe.mucahit;
 
 public class JavaInheritance {
 
+
     public static void main(String[] args) {
         System.out.println("Training will start soon");
+        System.out.println(" ");
     }
 }
