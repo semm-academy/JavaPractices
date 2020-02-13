8e2340936bc6b1d5110d986e84dd285ad934cabf
yusufdogan38 <yusufdgnyznfa@gmail.com>
2020-02-12 23:24:37 -0600

    DateAndTime

--- src/com/aktepe/mucahit/JavaDateAndTime.java
+++ src/com/aktepe/mucahit/JavaDateAndTime.java
@@ -1,11 +1,24 @@
 package com.aktepe.mucahit;
 
+
+import java.io.PrintStream;
+import java.time.LocalDate;
+
 public class JavaDateAndTime {
     /*
 
      */
 
     public static void main(String[] args) {
+
+       // LocalDate today = LocalDate.now(); System.out.println("Today's Local date : " + today);
+
+        LocalDate today = LocalDate.now();
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int day = today.getDayOfMonth();
+        final PrintStream printf = System.out.printf("Year : 1976 Month : 10 day : 10 \t %n", year, month, day);
+
 
     }
 }
